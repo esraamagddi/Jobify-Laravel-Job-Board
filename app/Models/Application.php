3249c49e7dd860224ec5bd881a037b6d797a4563
esraamagddi <esraamagdy20133@gmail.com>
2024-05-07 17:04:17 +0200

    feat:create application model

---
+++ app/Models/Application.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Queue\Job;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Js;
+
+class Application extends Model
+{
+    use HasFactory;
+    public function candidate()
+    {
+        return $this->belongsTo(Candidate::class);
+    }
+
+    public function job()
+    {
+        return $this->belongsTo(Job::class);
+    }
+
+}
