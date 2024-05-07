4e5ba442a9755022f8eca5b5dd1996f76ee922ae
EngAlaaKamal <alaa.kamal.ghonem@gmail.com>
2024-05-07 21:58:20 +0300

    add application model

---
+++ app/Models/Application.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Queue\Job;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Application extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'candidate_id',
+        'job_id',
+        'resume',
+        'contact_details',
+        'status',
+    ];
+    public function candidate()
+    {
+        return $this->belongsTo(Candidate::class);
+    }
+
+    public function job()
+    {
+        return $this->belongsTo(Job::class);
+    }
+}
