2059f89bb563e35670e9ba83aae3f22d9085c818
sara-aref <sara.aref.hassan@gmail.com>
2024-05-08 13:25:49 +0300

    Feat:Create Job model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Job;
 
 class Category extends Model
 {
     use HasFactory;
+    protected $fillable = ['name'];
+
+    public function job(): HasMany
+    {
+        return $this->hasMany(Job::class, 'category_id', 'id');
+    }
 }
---
+++ app/Models/Job.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Employer;
+use App\Models\Category;
+
+class Job extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'description', 'responsibilities', 'skills', 'qualifications', 'salary_range', 'work_type', 'location', 'deadline', 'employer_id', 'category_id'];
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class, 'employer_id', 'id');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
+}
