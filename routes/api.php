9bbc655abcfa8686d74352b5b251b1095789e300
esraamagddi <esraamagdy20133@gmail.com>
2024-05-09 04:38:13 +0200

    chore:remove unwanted comment

--- routes/api.php
+++ routes/api.php
@@ -9,5 +9,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-// Route::post('/applications', [ApplicationController::class, 'store']);
 Route::resource('applications', ApplicationController::class);
