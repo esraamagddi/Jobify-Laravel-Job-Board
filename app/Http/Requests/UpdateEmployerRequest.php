eb0d5a4f3de553c7281d5fa2aca2ee25258ed1e4
sara-aref <sara.aref.hassan@gmail.com>
2024-05-13 11:15:24 +0300

    add status column to posts table

---
+++ app/Http/Requests/UpdateEmployerRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateEmployerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'max:50|min:3',
+            'email' => 'unique:employers|email|max:50',
+            'industry' => 'max:50',
+            'password' => 'max:16|min:8|regex:/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+\-={}[\]:;"\'<>,.?\/]).{8,}$/',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.max'=>'maximum length for name is 50 letters',
+            'name.min' => 'name must be at least 3 characters long',
+            'email.unique' => 'this email is already exists',
+            'email.regex' => 'invalid email',
+            'password.max' => 'Password cannot be longer than 16 characters',
+            'password.min' => 'Password must be at least 8 characters',
+            'password.regex' => 'The password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character',
+            'industry.max'=>'maximum length for industry is 50 letters',
+            'logo.max'=> 'maximum length for logo name is 50 letters',
+        ];
+    }
+}
